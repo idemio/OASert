98ed52aae88bc11ef4296a607e869da7d7191d14
Kalev Gonvick <kalev.gonvick@outlook.com>
2025-05-23 19:41:10 -0700

    finished generic openapi validator

--- src/openapi/mod.rs
+++ src/openapi/mod.rs
@@ -1,17 +1,41 @@
 use crate::OpenApiValidationError;
 use crate::openapi_util::JsonPath;
-use serde_json::{Map, Value};
+use jsonschema::{Resource, ValidationError, ValidationOptions, Validator};
+use serde_json::{Map, Value, json};
 use std::collections::{HashMap, HashSet};
+use unicase::UniCase;
+
+fn vec_2_string(vec: &Vec<Value>) -> String {
+    let mut out = String::new();
+    for el in vec {
+        out.push_str(&el.to_string());
+    }
+    out
+}
 
 pub struct OpenApiValidatorV2 {
     traverser: OpenApiTraverser,
+    validator_options: ValidationOptions,
 }
 
 impl OpenApiValidatorV2 {
-    pub fn new(value: Value) -> Self {
-        Self {
+    pub fn new(mut value: Value) -> Result<Self, OpenApiValidationError> {
+        value["$id"] = json!("@@root");
+
+        let resource = match Resource::from_contents(value.clone()) {
+            Ok(res) => res,
+            Err(_) => {
+                return Err(OpenApiValidationError::InvalidSchema(
+                    "Invalid specification provided!".to_string(),
+                ));
+            }
+        };
+        let options = Validator::options().with_resource("@@inner", resource);
+
+        Ok(Self {
             traverser: OpenApiTraverser::new(value),
-        }
+            validator_options: options,
+        })
     }
 
     fn get_operation(&self, path: &str, method: &str) -> Option<(&Value, JsonPath)> {
@@ -70,51 +94,163 @@ impl OpenApiValidatorV2 {
 
     fn validate_body(
         &self,
-        endpoint_operation: &Value,
-        body: &Value,
-        headers: &HashMap<String, String>,
+        request_body_path: &JsonPath,
+        request_schema: &Value,
+        request_body: Option<&Value>,
     ) -> Result<(), OpenApiValidationError> {
-        todo!()
+        if let Err(e) = self.check_required_body(request_schema, request_body) {
+            return Err(e);
+        }
+        if let Some(body) = request_body {
+            return self.complex_validation(request_body_path, body);
+        }
+        Ok(())
     }
 
     fn check_required_body(
         &self,
         body_schema: &Value,
-        body: Option<&Value>,
+        request_body: Option<&Value>,
     ) -> Result<(), OpenApiValidationError> {
-        todo!()
+        if let Some(required_fields) = body_schema
+            .get("required")
+            .and_then(|required| required.as_array())
+        {
+            if !required_fields.is_empty() && request_body.is_none() {
+                return Err(OpenApiValidationError::InvalidRequest(format!(
+                    "Request schema contains required fields {} but provided request is empty.",
+                    vec_2_string(required_fields)
+                )));
+            }
+            for required in required_fields {
+                let required_field = required.as_str().unwrap();
+                if request_body.is_some_and(|body| body.get(required_field).is_none()) {
+                    return Err(OpenApiValidationError::InvalidRequest(format!(
+                        "Request body missing required field '{}'",
+                        required_field
+                    )));
+                }
+            }
+        }
+        Ok(())
     }
 
     fn validate_headers(
         &self,
-        request_params: &Vec<Value>,
-        headers: &HashMap<String, String>,
+        param_schemas: &Vec<Value>,
+        headers: Option<&HashMap<UniCase<String>, String>>,
     ) -> Result<(), OpenApiValidationError> {
-        todo!()
+        if let Err(e) = self.check_required_params(param_schemas, headers) {
+            return Err(e);
+        }
+
+        for param in param_schemas {
+            if param
+                .get("in")
+                .is_some_and(|param| param.as_str().is_some_and(|param| param == "header"))
+            {
+                let is_required = param
+                    .get("required")
+                    .and_then(|req| req.as_bool())
+                    .unwrap_or(false);
+                let name = param.get("name").and_then(|name| name.as_str()).unwrap();
+                let schema = param.get("schema").unwrap();
+                if let Some(header_value) =
+                    headers.and_then(|headers| headers.get(&UniCase::<String>::from(name)))
+                {
+                    if let Err(e) = Self::simple_validation(schema, &json!(header_value)) {
+                        return Err(e);
+                    }
+                } else if is_required {
+                    return Err(OpenApiValidationError::InvalidHeaders(format!(
+                        "Missing request header: '{}'",
+                        name
+                    )));
+                }
+            }
+        }
+        Ok(())
     }
 
-    fn check_required_header_params(
+    fn simple_validation(schema: &Value, instance: &Value) -> Result<(), OpenApiValidationError> {
+        if let Err(e) = jsonschema::validate(schema, instance) {
+            return Err(OpenApiValidationError::InvalidSchema(format!(
+                "Validation failed: {}",
+                e.to_string()
+            )));
+        }
+        Ok(())
+    }
+
+    fn complex_validation(
         &self,
-        request_params: &Vec<Value>,
-        headers: Option<&HashMap<String, String>>,
+        json_path: &JsonPath,
+        instance: &Value,
     ) -> Result<(), OpenApiValidationError> {
-        todo!()
+        let full_pointer_path = format!("@@root#/{}", json_path.format_path());
+        let schema = json!({
+            "$ref": full_pointer_path
+        });
+        let validator = match self.validator_options.build(&schema) {
+            Ok(val) => val,
+            Err(_) => {
+                return Err(OpenApiValidationError::InvalidSchema(format!(
+                    "Could not construct validator for json_path {}",
+                    json_path.format_path()
+                )));
+            }
+        };
+        match validator.validate(instance) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(OpenApiValidationError::InvalidRequest(format!(
+                "Schema validation failed: {}",
+                e.to_string()
+            ))),
+        }
+    }
+
+    fn check_required_params(
+        &self,
+        param_schemas: &Vec<Value>,
+        request_params: Option<&HashMap<UniCase<String>, String>>,
+    ) -> Result<(), OpenApiValidationError> {
+        if let Some(headers) = request_params {
+            for param in param_schemas {
+                let param_name = param.get("name").and_then(|name| name.as_str()).unwrap();
+                let section = param.get("in").and_then(|name| name.as_str()).unwrap();
+                let param_required = param
+                    .get("required")
+                    .and_then(|required| required.as_bool())
+                    .unwrap_or(false);
+                if !headers.contains_key(&UniCase::<String>::from(param_name)) && param_required {
+                    return Err(OpenApiValidationError::InvalidRequest(format!(
+                        "Request {} param missing '{}' ",
+                        section, param_name
+                    )));
+                }
+            }
+        }
+        Ok(())
     }
 
     fn validate_query_params(
         &self,
         request_params: &Vec<Value>,
-        query_params: &HashMap<String, String>,
+        query_params: Option<&HashMap<UniCase<String>, String>>,
     ) -> Result<(), OpenApiValidationError> {
+        if let Err(e) = self.check_required_params(request_params, query_params) {
+            return Err(e);
+        }
         todo!()
     }
 
-    fn check_required_query_params(
-        &self,
-        query_schemas: &Vec<Value>,
-        query_params: Option<&HashMap<String, String>>,
-    ) -> Result<(), OpenApiValidationError> {
-        todo!()
+    fn extract_content_type(headers: Option<&HashMap<UniCase<String>, String>>) -> Option<&str> {
+        if let Some(headers) =
+            headers.and_then(|headers| headers.get(&UniCase::from("content-type")))
+        {
+            return Some(headers);
+        }
+        None
     }
 
     pub fn validate_request(
@@ -122,11 +258,10 @@ impl OpenApiValidatorV2 {
         path: &str,
         method: &str,
         body: Option<&Value>,
-        headers: Option<&HashMap<String, String>>,
-        query_params: Option<&HashMap<String, String>>,
-        scopes: Option<&HashSet<String>>,
+        headers: Option<&HashMap<UniCase<String>, String>>,
+        query_params: Option<&HashMap<UniCase<String>, String>>,
     ) -> Result<(), OpenApiValidationError> {
-        let (operation, path) = match self.get_operation(path, method) {
+        let (operation, mut path) = match self.get_operation(path, method) {
             None => {
                 return Err(OpenApiValidationError::InvalidPath(
                     "No path found".to_string(),
@@ -135,30 +270,33 @@ impl OpenApiValidatorV2 {
             Some(val) => (val.0, val.1),
         };
 
-        // TODO - get request body schema, headers, query params, path params first. then validate between them
-        let request_schema: Option<&Value> = self.traverser.get_request_body(operation);
-        let request_params: Option<&Vec<Value>> = self.traverser.get_request_parameters(operation);
-        let request_security: Option<&Vec<Value>> = self.traverser.get_request_security(operation);
-
-        if let Err(e) = match (body, headers, request_schema) {
-            (Some(body), Some(headers), Some(request_body_schema)) => {
-                self.validate_body(request_body_schema, body, headers)
+        let request_schema = match Self::extract_content_type(headers) {
+            None => None,
+            Some(content_type) => {
+                path.add_segment("requestBody".to_string());
+                path.add_segment(content_type.to_string());
+                path.add_segment("schema".to_string());
+                self.traverser.get_request_body(operation, content_type)
             }
-            (Some(_), None, Some(_)) => Err(OpenApiValidationError::InvalidRequest(
-                "Request contains body but no content-type".to_string(),
-            )),
-            (Some(_), _, None) => Err(OpenApiValidationError::InvalidRequest(
+        };
+        let request_params = self.traverser.get_request_parameters(operation);
+
+        if let Err(e) = match (body.is_some(), request_schema) {
+            (true, Some(request_body_schema)) => {
+                self.validate_body(&path, request_body_schema, body)
+            }
+            (true, None) => Err(OpenApiValidationError::InvalidRequest(
                 "Request body provided when endpoint has no request schema defined".to_string(),
             )),
-            (_, _, _) => Ok(()),
+            (_, _) => Ok(()),
         } {
             return Err(e);
         }
 
-        if let Err(e) = match (headers, request_params) {
-            (Some(headers), Some(request_params)) => self.validate_headers(request_params, headers),
-            (None, Some(request_params)) => self.check_required_header_params(request_params, None),
-            (Some(_), None) => Err(OpenApiValidationError::InvalidRequest(
+        if let Err(e) = match (headers.is_some(), request_params) {
+            (true, Some(request_params)) => self.validate_headers(request_params, headers),
+            (false, Some(request_params)) => self.check_required_params(request_params, None),
+            (true, None) => Err(OpenApiValidationError::InvalidRequest(
                 "Request parameters provided when endpoint has no parameters defined".to_string(),
             )),
             (_, _) => Ok(()),
@@ -166,12 +304,12 @@ impl OpenApiValidatorV2 {
             return Err(e);
         }
 
-        if let Err(e) = match (query_params, request_params) {
-            (Some(query), Some(request_params)) => {
-                self.validate_query_params(request_params, query)
+        if let Err(e) = match (query_params.is_some(), request_params) {
+            (true, Some(request_params)) => {
+                self.validate_query_params(request_params, query_params)
             }
-            (None, Some(request_params)) => self.check_required_query_params(request_params, None),
-            (Some(_), None) => Err(OpenApiValidationError::InvalidRequest(
+            (false, Some(request_params)) => self.check_required_params(request_params, None),
+            (true, None) => Err(OpenApiValidationError::InvalidRequest(
                 "Query parameters provided when endpoint has no parameters defined".to_string(),
             )),
             (_, _) => Ok(()),
@@ -179,7 +317,7 @@ impl OpenApiValidatorV2 {
             return Err(e);
         }
 
-        todo!()
+        Ok(())
     }
 }
 
@@ -196,16 +334,36 @@ impl OpenApiTraverser {
         }
     }
 
-    pub fn get_request_body(&self, operation: &Value) -> Option<&Value> {
-        todo!()
+    pub fn get_request_body<'a>(
+        &'a self,
+        operation: &'a Value,
+        content_type: &'a str,
+    ) -> Option<&'a Value> {
+        if let Some(schema) = Self::get(&self.specification, operation, "requestBody")
+            .and_then(|node| Self::get(&self.specification, node, content_type))
+            .and_then(|node| Self::get(&self.specification, node, "schema"))
+        {
+            return Some(schema);
+        }
+        None
     }
 
-    pub fn get_request_parameters(&self, operation: &Value) -> Option<&Vec<Value>> {
-        todo!()
+    pub fn get_request_parameters<'a>(&'a self, operation: &'a Value) -> Option<&'a Vec<Value>> {
+        if let Some(parameters) =
+            Self::get(&self.specification, operation, "parameters").and_then(|node| node.as_array())
+        {
+            return Some(parameters);
+        }
+        None
     }
 
-    pub fn get_request_security(&self, operation: &Value) -> Option<&Vec<Value>> {
-        todo!()
+    pub fn get_request_security<'a>(&'a self, operation: &'a Value) -> Option<&'a Vec<Value>> {
+        if let Some(security) =
+            Self::get(&self.specification, operation, "security").and_then(|node| node.as_array())
+        {
+            return Some(security);
+        }
+        None
     }
 
     pub fn get_from_spec_as_map<'a>(
@@ -342,16 +500,18 @@ impl OpenApiTraverser {
 
 #[cfg(test)]
 mod test {
+    use std::collections::HashMap;
     use crate::openapi::OpenApiValidatorV2;
     use crate::openapi_util::JsonPath;
-    use serde_json::Value;
+    use serde_json::{Value, json};
     use std::fs;
+    use unicase::UniCase;
 
     #[test]
     fn test_find_operation() {
         let spec_string = fs::read_to_string("./test/openapi.json").unwrap();
         let specification: Value = serde_json::from_str(&spec_string).unwrap();
-        let validator = OpenApiValidatorV2::new(specification);
+        let validator = OpenApiValidatorV2::new(specification).unwrap();
         let result: Option<(&Value, JsonPath)> =
             validator.get_operation("/pet/findByStatus/MultipleExamples", "get");
         assert!(result.is_some());
@@ -361,5 +521,39 @@ mod test {
             "paths/~1pet~1findByStatus~1MultipleExamples/get",
             result.1.format_path()
         )
+    }
+
+    #[test]
+    fn test_validate_request() {
+        let spec_string = fs::read_to_string("./test/openapi.json").unwrap();
+        let specification: Value = serde_json::from_str(&spec_string).unwrap();
+        let validator = OpenApiValidatorV2::new(specification).unwrap();
+        let example_request_body = json!({
+          "id": 1,
+          "category": {
+            "id": 1,
+            "name": "cat"
+          },
+          "name": "fluffy",
+          "photoUrls": [
+            "http://example.com/path/to/cat/1.jpg",
+            "http://example.com/path/to/cat/2.jpg"
+          ],
+          "tags": [
+            {
+              "id": 1,
+              "name": "cat"
+            }
+          ],
+          "status": "available"
+        });
+
+        let mut example_headers: HashMap<UniCase<String>, String> = HashMap::new();
+        example_headers.insert(UniCase::from("Content-Type"), "application/json".to_string());
+        let path = "/pet";
+        let method = "post";
+        let result = validator.validate_request(path, method, Some(&example_request_body), Some(&example_headers), None);
+
+        assert!(result.is_ok());
     }
 }
